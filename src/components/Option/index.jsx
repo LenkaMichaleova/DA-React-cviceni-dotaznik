ff5ae5af9adf6e55b3dc591cbce7b1e1a376a2af
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-05-14 07:12:08 +0200

    vyřešena druhá část úkolu - po kliknutí na odpověď se zobrazí symbol fajfky

--- src/components/Option/index.jsx
+++ src/components/Option/index.jsx
@@ -1,9 +1,9 @@
 import { Icon } from '../Icon/index';
 import './style.css';
 
-export const Option = ({type, text}) => {
+export const Option = ({type, text, onSelected}) => {
   return (
-    <div className="option">
+    <div className="option" onClick={() => onSelected()}>
       <Icon type={type} />
       <div>{text}</div>
     </div>
--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -1,20 +1,27 @@
 import { QuestionBody } from '../QuestionBody/index';
 import { Option } from '../Option/index';
 import './style.css';
+import { useState } from 'react';
 
 export const Question = ({text}) => {
+  const [answered, setAnswered] = useState(false)
+
+  const handleSelect = () => {
+    setAnswered(!answered)
+  }
+
   return (
     <div className="question">
       <QuestionBody 
-        iconType="symbolQuestion" 
+        iconType={answered ? "symbolTick" : "symbolQuestion"} 
         text={text}
       />
       <div className="question__options">
-        <Option type="smileyStrongYes" text="Souhlasím"/>
-        <Option type="smileyYes" text="Spíše souhlasím"/>
-        <Option type="smileyNeutral" text="Nevím"/>
-        <Option type="smileyNo" text="Spíše nesouhlasím"/>
-        <Option type="smileyStrongNo" text="Nesouhlasím"/>
+        <Option type="smileyStrongYes" text="Souhlasím" onSelected={handleSelect}/>
+        <Option type="smileyYes" text="Spíše souhlasím" onSelected={handleSelect}/>
+        <Option type="smileyNeutral" text="Nevím" onSelected={handleSelect}/>
+        <Option type="smileyNo" text="Spíše nesouhlasím" onSelected={handleSelect}/>
+        <Option type="smileyStrongNo" text="Nesouhlasím" onSelected={handleSelect}/>
       </div>
     </div>
   );
